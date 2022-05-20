1db638603a4a44c43a3d7aa1645bd02941050c2b
jamir <jamirvillarroel@gmail.com>
2022-05-20 14:46:01 -0500

    Vector ordenado

--- ord_burbuja.c
+++ ord_burbuja.c
@@ -5,12 +5,24 @@
 #define SIZE 10
 
 int main(){
+    int aux;
     int ord_vector[SIZE];
+    int i, j;
+
     srand(time(NULL));
     printf("Generacion de numeros aleatorios");
-    for (int i = 0; i < SIZE; i++){
+    for (i = 0; i < SIZE; i++){
         ord_vector[i] = 100 + rand()%(101);
         printf("\n");
         printf("%d", ord_vector[i]);
         }
+    for (i=0;i<SIZE;i++){
+        for(j=0;j<SIZE - i - 1; j++){
+            if(ord_vector[j]>ord_vector[j+1]){
+                aux = ord_vector[j];
+                ord_vector[j]= ord_vector[j+1];
+                ord_vector[j+1]= aux;
+            }
+        }
+    }    
 }
